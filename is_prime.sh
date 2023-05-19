1334979cbb05af4bab07fac9b2929a240027422e
“Chetankoli02” <chetankoli2016@gmail.com>
2023-05-19 16:53:04 +0530

    Add: prime number problem

---
+++ is_prime.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read n
+
+if [[ $n -lt 2 ]]; then
+    echo "$n is not a prime number"
+    exit
+fi
+
+for (( i=2; i<=$n/2; i++ ))
+do
+    if [[ $n%$i -eq 0 ]]; then
+        echo "$n is not a prime number"
+        exit
+    fi
+done
+
+echo "$n is a prime number"
